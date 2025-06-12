70c5625e5e25302d9cd17de037299803ef32bb3f
Kirk Mendoza <kirkmendoza@gmail.com>
2025-06-12 00:16:20 -0500

    Set up seed()

---
+++ db.js
@@ -0,0 +1,22 @@
+const pg = require('pg')
+const client = new pg.Client('postgres://localhost/the_acme_store' || process.env.DATABASE_URL)
+
+const seed = () => {
+    // CREATE TABLES
+    const SQL = `
+        CREATE TABLE users(
+            id UUID PRIMARY KEY,
+            username VARCHAR(100) UNIQUE NOT NULL,
+            password VARCHAR(100) NOT NULL
+        );
+        CREATE TABLE products(
+            id UUID PRIMARY KEY,
+            name VARCHAR(100) NOT NULL
+        );
+        CREATE TABLE favorites(
+            id UUID PRIMARY KEY,
+            product_id UUID REFERENCES products(id) NOT NULL,
+            user_id UUID REFERENCES users(id) NOT NULL
+        );
+    `
+}
